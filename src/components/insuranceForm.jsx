9a005eb1c1171e0fb4a13b9fd156e11f1b8d18b5
Muhammad Shahbaz Rafique <97734247+Shahbaz-Rafique@users.noreply.github.com>
2023-12-28 15:54:22 -0800

    commit

--- src/components/insuranceForm.jsx
+++ src/components/insuranceForm.jsx
@@ -11,7 +11,7 @@ const InsuranceForm = () => {
     const [options,setOptions]=useState([]);
 
     useEffect(()=>{
-        const url = 'http://localhost:4000/getinsurer'; 
+        const url = 'https://first-verbena-galette.glitch.me/getinsurer'; 
         fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -57,7 +57,7 @@ const InsuranceForm = () => {
         const nric=document.getElementById("nric").value;
         const mobile=document.getElementById("mobile").value;
 
-        const url = 'http://localhost:4000/quotation';
+        const url = 'https://first-verbena-galette.glitch.me/quotation';
 
         const data = {
             regNo: regno,
@@ -185,7 +185,7 @@ const InsuranceForm = () => {
                         </div>
                     </div>
                 </div>
-                <div className=" flex justify-start items-start gap-4 ">
+                <div className=" flex justify-start items-start gap-4 " style={{marginTop:"10px"}}>
                     <input type="checkbox" className="w-6 h-6 bg-[#F9FAFB]" required/>
                     <div className="flex flex-col justify-start items-start gap-2 ">
                         <div>
@@ -196,7 +196,7 @@ const InsuranceForm = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex items-start">
+                <div className="flex items-start" style={{marginTop:"20px"}}>
                 <button
                     type="submit"
                     className={` px-4 py-2 text-sm w-fit flex gap-2 text-slate-50 items-center whitespace-nowrap bg-green-500 rounded-full font-medium self-center`}
